e44b9a507f4488c256f99190511527329667cca3
Jonathan Kaczynski <jon@knewton.com>
2014-03-05 09:55:31 -0500

    some fixes

--- setup.py
+++ setup.py
@@ -3,6 +3,17 @@ import os.path
 import re
 from setuptools import Command, find_packages, setup
 
+class PyTest(Command):
+	user_options = []
+	def initialize_options(self):
+		pass
+	def finalize_options(self):
+		pass
+	def run(self):
+		import sys, subprocess
+		errno = subprocess.call([sys.executable, "runtests.py"])
+		raise SystemExit(errno)
+
 def parse_requirements(file_name):
 	"""Taken from http://cburgmer.posterous.com/pip-requirementstxt-and-setuppy"""
 	requirements = []
@@ -20,11 +31,13 @@ setup(
 	url = "https://github.com/Knewton/ssh_helper",
 	author = "Devon Jones",
 	author_email = "devon@knewton.com",
-	license = "Proprietary",
+	license = "Apache Software License",
 	scripts = ['bin/ssh_helper', 'bin/ssh_helper_aliases.sh'],
-	packages=find_packages(),
+	packages = find_packages(),
+	cmdclass = {"test": PyTest},
 	package_data = {"config": ["requirements.txt"]},
-	install_requires = parse_requirements("requirements.txt")
-	description = "SSH helper for working with aws.",
+	install_requires = parse_requirements("requirements.txt"),
+	tests_require = parse_requirements("requirements.txt"),
+	description = "SSH helper for working with Amazon Web Services",
 	long_description = "\n" + open("README.md").read(),
 )
---
+++ ssh_helper/tests/test_ssh_helper.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestSshHelper(unittest.TestCase):
+
+	def setUp(self):
+		pass
+
+	def tearDown(self):
+		pass
+
+	def test_noop(self):
+		pass
+
